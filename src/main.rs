f35f4d0e47a3f3c47d3433e56d6c89ef5e622f49
Italo Guajardo <italog.penaloza@gmail.com>
2022-12-14 13:44:12 -0300

    move player with arrow keys

--- src/main.rs
+++ src/main.rs
@@ -23,8 +23,10 @@ struct State {
 impl GameState for State {
     fn tick(&mut self, ctx: &mut Rltk) {
         ctx.cls();
-        
+
         self.run_systems();
+
+        player_input(self, ctx);
 
         let positions = self.ecs.read_storage::<Position>();
         let renderables = self.ecs.read_storage::<Renderable>();
@@ -55,11 +57,39 @@ impl<'a> System<'a> for LeftWalker {
     type SystemData = (ReadStorage<'a, LeftMover>, WriteStorage<'a, Position>);
 
     fn run(&mut self, (lefty, mut pos): Self::SystemData) {
+        // Get all the leftMover and with position then move them 1 position to the left
         for (_lefty, pos) in (&lefty, &mut pos).join() {
             pos.x -= 1;
             if pos.x < 0 {
                 pos.x = 79;
             }
+        }
+    }
+}
+
+#[derive(Component, Debug)]
+struct Player {}
+
+fn try_move_player(delta_x: i32, delta_y: i32, ecs: &mut World) {
+    let mut positions = ecs.write_storage::<Position>();
+    let mut players = ecs.write_storage::<Player>();
+    
+    for (_player, pos) in (&mut players, &mut positions).join() {
+        pos.x = min( 79, max(0, pos.x + delta_x));
+        pos.y = min( 49, max(0, pos.y + delta_y));
+        
+    }
+}
+
+fn player_input(gs: &mut State, ctx: &mut Rltk) {
+    match ctx.key {
+        None => {},
+        Some(key) => match key {
+            VirtualKeyCode::Left => try_move_player(-1, 0, &mut gs.ecs),
+            VirtualKeyCode::Right => try_move_player(1, 0, &mut gs.ecs),
+            VirtualKeyCode::Up => try_move_player(0, -1, &mut gs.ecs),
+            VirtualKeyCode::Down => try_move_player(0, 1, &mut gs.ecs),
+            _ => {}
         }
     }
 }
@@ -74,6 +104,7 @@ fn main() -> rltk::BError {
     gs.ecs.register::<Position>();
     gs.ecs.register::<Renderable>();
     gs.ecs.register::<LeftMover>();
+    gs.ecs.register::<Player>();
 
     gs.ecs
         .create_entity()
@@ -83,6 +114,7 @@ fn main() -> rltk::BError {
             fg: RGB::named(rltk::YELLOW),
             bg: RGB::named(rltk::BLACK),
         })
+        .with(Player {})
         .build(); // .build() takes the assembled entity and does the hard part - actually putting together all of the disparate parts into the right parts of the ECS for you.
 
     // The 0..10  is a range - and offers an iterator for Rust to navigate
