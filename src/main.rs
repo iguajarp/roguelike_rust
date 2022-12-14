eb0eb5086a08a616e9c93e00b430dbcd4d83da03
Italo Guajardo <italog.penaloza@gmail.com>
2022-12-13 21:22:18 -0300

    add Renderable and Position

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,20 @@
-use rltk::{GameState, Rltk};
+use rltk::{GameState, Rltk, RGB, VirtualKeyCode}; // extern is an old keyword, not needed now
+use specs::prelude::*; // macro_use is an old keyword too. Not needed anymore
+use std::cmp::{max, min};
+use specs_derive::Component;
+
+#[derive(Component)] // The #[macro_use] use specs_derive::Component; earliers versions
+struct Position { // Just a POD, plain old data, is common for pure ECS. No logic
+    x: i32,
+    y: i32,
+}
+
+#[derive(Component)]
+struct Renderable {
+    glyph: rltk::FontCharType,
+    fg: RGB,
+    bg: RGB,
+}
 
 struct State {}
 impl GameState for State {
