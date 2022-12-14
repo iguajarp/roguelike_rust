4130ad9dd6ee3d39a143a1be05bdcd9f0743a408
Italo Guajardo <italog.penaloza@gmail.com>
2022-12-13 21:51:16 -0300

    add entities to the context

--- src/main.rs
+++ src/main.rs
@@ -23,7 +23,15 @@ struct State {
 impl GameState for State {
     fn tick(&mut self, ctx: &mut Rltk) {
         ctx.cls();
-        ctx.print(1, 1, "Hello Rust World");
+        
+        let positions = self.ecs.read_storage::<Position>();
+        let renderables = self.ecs.read_storage::<Renderable>();
+
+        // You need a Position to know where to draw, and Renderable to know what to draw!
+        // Join return only entities that have both, like a db INNER JOIN. Works with tuples
+        for (pos, render) in (&positions, &renderables).join() {
+            ctx.set(pos.x, pos.y, render.fg, render.bg, render.glyph);
+        }
     }
 }
 
@@ -47,6 +55,7 @@ fn main() -> rltk::BError {
         })
         .build(); // .build() takes the assembled entity and does the hard part - actually putting together all of the disparate parts into the right parts of the ECS for you.
 
+        // The 0..10  is a range - and offers an iterator for Rust to navigate
         for i in 0..10 {
             gs.ecs
             .create_entity()
