9ca91c46fd66a19a7d3dff5719a45a8d5358291b
Italo Guajardo <italog.penaloza@gmail.com>
2022-12-14 21:10:56 -0300

    create map code and new map random generator

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use rltk::{GameState, Rltk, VirtualKeyCode, RGB}; // extern is an old keyword, not needed now
+use rltk::{GameState, Rltk, VirtualKeyCode, RGB, Tile}; // extern is an old keyword, not needed now
 use specs::prelude::*; // macro_use is an old keyword too. Not needed anymore
 use specs_derive::Component;
 use std::cmp::{max, min};
@@ -92,6 +92,48 @@ fn player_input(gs: &mut State, ctx: &mut Rltk) {
             _ => {}
         }
     }
+}
+
+// Clone override the default move behavior on assignment
+// PartialEq allows == . Ex: tile_type == TileType::Wall
+#[derive(PartialEq, Copy, Clone)] 
+enum TileType {
+    Wall,
+    Floor,
+}
+
+pub fn xy_idx(x: i32, y: i32) -> usize {
+    (y as usize * 80) + x as usize
+}
+
+fn new_map() -> Vec<TileType> {
+    let mut map = vec![TileType::Floor; 80*50];
+
+    // Make the boundaries walls
+    for x in 0..80 {
+        map[xy_idx(x, 0)] = TileType::Wall;
+        map[xy_idx(x, 49)] = TileType::Wall;
+    }
+
+    for y in 0..50 {
+        map[xy_idx(0, y)] = TileType::Wall;
+        map[xy_idx(79, y)] = TileType::Wall;
+    }
+
+    // Now we'll randomly splat a bunch of walls. It won't be pretty, but it's a decent illustration.
+    // First, obtain the thread-local RNG:
+    let mut rng = rltk::RandomNumberGenerator::new();
+
+    for _i in 0..400 {
+        let x = rng.roll_dice(1, 79);
+        let y = rng.roll_dice(1, 79);
+        let idx = xy_idx(x, y);
+        if idx != xy_idx(40, 25) {
+            map[idx] = TileType::Wall;
+        }
+    }
+
+    map
 }
 
 fn main() -> rltk::BError {
