8c29251689f4d7d7962433843dd5d094eeda2c9a
Italo Guajardo <italog.penaloza@gmail.com>
2022-12-15 12:24:19 -0300

    create multiple rooms upon the map

--- src/map.rs
+++ src/map.rs
@@ -1,12 +1,14 @@
-use crate::{TileType, xy_idx, rect::Rect};
+use rltk::RandomNumberGenerator;
 
+use crate::{rect::Rect, xy_idx, TileType};
+use std::cmp::{max, min};
 
 /// It defines a map using a Vec<TileType>. So, it doesn't use cartesanian coor.
 /// But index from 0 to the area of the map.
 /// It places walls around the outer edges of the map, and then adds 400 random
 /// walls anywhere that isn't the player's starting point.
 pub fn new_map_test() -> Vec<TileType> {
-    let mut map = vec![TileType::Floor; 80*50];
+    let mut map = vec![TileType::Floor; 80 * 50];
 
     // Make the boundaries walls
     for x in 0..80 {
@@ -37,21 +39,59 @@ pub fn new_map_test() -> Vec<TileType> {
 pub fn new_map_rooms_and_corridors() -> Vec<TileType> {
     let mut map = vec![TileType::Wall; 80 * 50];
 
-    let room1 = Rect::new(20, 15, 10, 15);
-    let room2 = Rect::new(35, 15, 10, 15);
+    let mut rooms: Vec<Rect> = Vec::new();
+    const MAX_ROOMS: i32 = 30;
+    const MIN_SIZE: i32 = 6;
+    const MAX_SIZE: i32 = 10;
 
-    apply_room_to_map(&room1, &mut map);
-    apply_room_to_map(&room2, &mut map);
+    let mut rng = RandomNumberGenerator::new();
 
+    for _ in 0..MAX_ROOMS {
+        let w = rng.range(MIN_SIZE, MAX_SIZE);
+        let h = rng.range(MIN_SIZE, MAX_SIZE);
+
+        // - w is for not surpassing the width of the map. Same for y
+        let x = rng.roll_dice(1, 80 - w - 1);
+        let y = rng.roll_dice(1, 50 - h - 1);
+        let new_room = Rect::new(x, y, w, h);
+        let mut ok = true;
+
+        for other_room in rooms.iter() {
+            if new_room.intersect(other_room) {
+                ok = false
+            }
+        }
+        if ok {
+            apply_room_to_map(&new_room, &mut map);
+            rooms.push(new_room);
+        }
+    }
 
     map
 }
 
-
 fn apply_room_to_map(room: &Rect, map: &mut [TileType]) {
-    for y in room.y1 +1 ..= room.y2 {
-        for x in room.x1 + 1 ..= room.x2 {
+    for y in room.y1 + 1..=room.y2 {
+        for x in room.x1 + 1..=room.x2 {
             map[xy_idx(x, y)] = TileType::Floor;
+        }
+    }
+}
+
+fn apply_horizontal_tunnel(map: &mut [TileType], x1: i32, x2: i32, y: i32) {
+    for x in min(x1, x2)..=max(x1, x2) {
+        let idx = xy_idx(x, y);
+        if idx > 0 && idx < 80 * 50 {
+            map[idx as usize] = TileType::Floor;
+        }
+    }
+}
+
+fn apply_vertical_tunnel(map: &mut [TileType], y1: i32, y2: i32, x: i32) {
+    for y in min(y1, y2)..=max(y1, y2) {
+        let idx = xy_idx(x, y);
+        if idx > 0 && idx < 80 * 50 {
+            map[idx as usize] = TileType::Floor;
         }
     }
 }
