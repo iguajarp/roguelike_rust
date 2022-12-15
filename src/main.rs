410cc5c9f426020209aec2b9cfa5797df6d0f211
Italo Guajardo <italog.penaloza@gmail.com>
2022-12-14 22:14:07 -0300

    draw map function

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use rltk::{GameState, Rltk, VirtualKeyCode, RGB, Tile}; // extern is an old keyword, not needed now
+use rltk::{GameState, Rltk, Tile, VirtualKeyCode, RGB}; // extern is an old keyword, not needed now
 use specs::prelude::*; // macro_use is an old keyword too. Not needed anymore
 use specs_derive::Component;
 use std::cmp::{max, min};
@@ -36,13 +36,12 @@ impl GameState for State {
         for (pos, render) in (&positions, &renderables).join() {
             ctx.set(pos.x, pos.y, render.fg, render.bg, render.glyph);
         }
-
     }
 }
 
 impl State {
     fn run_systems(&mut self) {
-        let mut lw = LeftWalker{};
+        let mut lw = LeftWalker {};
         lw.run_now(&self.ecs);
         self.ecs.maintain();
     }
@@ -73,30 +72,29 @@ struct Player {}
 fn try_move_player(delta_x: i32, delta_y: i32, ecs: &mut World) {
     let mut positions = ecs.write_storage::<Position>();
     let mut players = ecs.write_storage::<Player>();
-    
+
     for (_player, pos) in (&mut players, &mut positions).join() {
-        pos.x = min( 79, max(0, pos.x + delta_x));
-        pos.y = min( 49, max(0, pos.y + delta_y));
-        
+        pos.x = min(79, max(0, pos.x + delta_x));
+        pos.y = min(49, max(0, pos.y + delta_y));
     }
 }
 
 fn player_input(gs: &mut State, ctx: &mut Rltk) {
     match ctx.key {
-        None => {},
+        None => {}
         Some(key) => match key {
             VirtualKeyCode::Left => try_move_player(-1, 0, &mut gs.ecs),
             VirtualKeyCode::Right => try_move_player(1, 0, &mut gs.ecs),
             VirtualKeyCode::Up => try_move_player(0, -1, &mut gs.ecs),
             VirtualKeyCode::Down => try_move_player(0, 1, &mut gs.ecs),
             _ => {}
-        }
+        },
     }
 }
 
 // Clone override the default move behavior on assignment
 // PartialEq allows == . Ex: tile_type == TileType::Wall
-#[derive(PartialEq, Copy, Clone)] 
+#[derive(PartialEq, Copy, Clone)]
 enum TileType {
     Wall,
     Floor,
@@ -112,7 +110,7 @@ pub fn xy_idx(x: i32, y: i32) -> usize {
 /// It places walls around the outer edges of the map, and then adds 400 random
 /// walls anywhere that isn't the player's starting point.
 fn new_map() -> Vec<TileType> {
-    let mut map = vec![TileType::Floor; 80*50];
+    let mut map = vec![TileType::Floor; 80 * 50];
 
     // Make the boundaries walls at the borders of the map
     for x in 0..80 {
@@ -141,6 +139,42 @@ fn new_map() -> Vec<TileType> {
     }
 
     map
+}
+
+// &[TileType] allows to receive a slices. And receive them as a reference.
+fn draw_map(map: &[TileType], ctx: &mut Rltk) {
+    let mut y = 0;
+    let mut x = 0;
+
+    for tile in map.iter() {
+        // Render the tile depending upon the tile type
+        match tile {
+            TileType::Floor => ctx.set(
+                x,
+                y,
+                RGB::from_f32(0.5, 0.5, 0.5),
+                RGB::from_f32(0., 0., 0.),
+                rltk::to_cp437('.'),
+            ),
+            TileType::Wall => {
+                ctx.set(
+                    x,
+                    y,
+                    RGB::from_f32(0.0, 1.0, 0.0),
+                    RGB::from_f32(0., 0., 0.),
+                    rltk::to_cp437('#'),
+                );
+            }
+        }
+
+        // Move the coordinates
+        // Needed to work with index instead of vector(x, y)
+        x += 1;
+        if x > 79 {
+            x = 0;
+            y += 1;
+        }
+    }
 }
 
 fn main() -> rltk::BError {
